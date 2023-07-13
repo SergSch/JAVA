09482778cac250b566a36477a0ac55bfb7510f34
Serg <serega101617@gmail.com>
2023-07-13 19:09:26 +0200

    added HW9

--- HW/HW9/src/main/java/HW/Task1.java
+++ HW/HW9/src/main/java/HW/Task1.java
@@ -1,2 +1,32 @@
-package HW;public class Task1 {
-}
+package HW;
+
+import java.util.Random;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[8];
+        Random random = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(50) + 1;
+            if (i == arr.length - 1) {
+                System.out.println(arr[i] + " ");
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 1) {
+                arr[i] = 0;
+            };
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.println(arr[i] + " ");
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        }
+    }
--- HW/HW9/src/main/java/HW/Task2.java
+++ HW/HW9/src/main/java/HW/Task2.java
@@ -1,2 +1,31 @@
-package HW;public class Task2 {
+package HW;
+
+import java.util.Random;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Random random = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(99) + 5;
+            if (i == arr.length - 1) {
+                System.out.println(arr[i] + " ");
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        boolean max = true;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                max = false;
+                break;
+            }
+        }
+        if (max) {
+            System.out.println("Массив строго возрастающей последовательностью");
+        } else {
+            System.out.println("Массив не строго возрастающей последовательностью");
+        }
+    }
 }
